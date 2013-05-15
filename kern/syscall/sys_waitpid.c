02627b091422811cfc8ff5f68f4c0ec29f7a5e6e
ginobuzz <ggbuzzelli@gmail.com>
2013-05-14 21:50:11 -0400

    Optimized wait_pid

--- kern/syscall/sys_waitpid.c
+++ kern/syscall/sys_waitpid.c
@@ -1,56 +1,59 @@
+/**************************************************************************
+ * wait_pid
+ *
+ * Input:
+ *		pid 	- PID of target process.
+ *		status* - Return value; status of exited process.
+ *		options - Only accepts 0 argument.
+ *		retval* - Set to target PID if successful.
+ * 
+ * Output:
+ *		pid_t 	- 0 if successful. Error code if failed.
+ *
+ * Description:
+ *		Waits for the process with the target PID if interested. After the
+ *		target process exits - destroys it.
+ *		Only interested if the target is the direct child of the current
+ *		process. 
+ *
+ * Last Modified: 5/14/2013 by ginobuzz
+ *
+ **************************************************************************/
 #include <kern/wait.h>
 #include <syscall.h>
 
 
+
 pid_t
 sys_waitpid(pid_t pid, int* status, int options, int* retval) {
+	*status = -1;
 
-
-	if (options != 0) return EINVAL;
+	/* Check input */
+	if (options != 0) 	return EINVAL;
 	if (status == NULL) return EFAULT;
+	if (pid == 0)		return ESRCH;
 
 
-	struct proc* proc = get_proc(pid);
-
-	// Does process exist?
-	if(proc == NULL) {
-		*status = -1;
-		return ESRCH;//Process DNE.
-	}
+	/* Get reference to targeted process */
+	struct proc* target = get_proc(pid);
+	if (target == NULL)	return ESRCH;
 
 
-	// Check for pid 0.
-	if(pid == 0){
-		*status = -1;
-		return ESRCH;
-	}
+	/* Check if interested in target process */
+	if(target->ppid != curthread->process->pid) return ECHILD;
 
 
-	// Are we are interested?
-	if(proc->ppid != curthread->process->pid) {
-		*status = -1;
-		return ECHILD;//Not interested.
-	}
-
-
-	// Is the process still alive?
-	if(proc->status == STATUS_ALIVE) {
-			P(proc->sem);// wait until it exits.
-	}
-	KASSERT(proc->status == STATUS_EXITED);
+	/* Wait until target process exits */
+	if(target->status == STATUS_ALIVE) P(target->sem);
+	KASSERT(target->status == STATUS_EXITED);
 	
-	int sts = _MKWAIT_EXIT(proc->exit_code);
-
-	int err = copyout(&sts, (userptr_t)status, sizeof(int));
-	if(err){
-		return err;
-	}
-
-
-	proc_destroy(pid);
-
-
+	/* Set results */
+	*status = _MKWAIT_EXIT(target->exit_code);
 	*retval = pid;
 
+	/* Destroy target process */
+	proc_destroy(pid);
+
+	
 	return 0;
 }
