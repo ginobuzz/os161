0aa4aa1919b0cda0497f14471c16ca5859658976
ginobuzz <ggbuzzelli@gmail.com>
2013-05-15 16:02:57 -0400

    Optimized exit, waitpid, and getpid

--- kern/syscall/sys_exit.c
+++ kern/syscall/sys_exit.c
@@ -1,23 +1,28 @@
+/**************************************************************************
+ * exit
+ *
+ * Input:
+ *		exitcode - exit code of the exiting process.
+ *
+ * Description:
+ *		Causes the current thread's process to exit.
+ *
+ * Last Modified: 5/15/2013 by ginobuzz
+ *
+ **************************************************************************/
 #include <syscall.h>
 #include <kern/wait.h>
 
 
 void
 sys_exit(int exitcode) {
-/* Causes the current process to exit. 
-*/
-
-	KASSERT(curthread->t_addrspace != NULL);
 
 	struct proc* proc = curthread->process;
+	KASSERT(proc != NULL);
 
-	//Set exitcode.
-	proc->exit_code = exitcode;
+	proc->exit_code  = exitcode;
+	proc->status 	= STATUS_EXITED;
 
-	// Change status.
-	proc->status = STATUS_EXITED;
-
-	// Increment processes semaphore, releasing waiting processes.
 	V(proc->sem);
 
 	thread_exit();
--- kern/syscall/sys_getpid.c
+++ kern/syscall/sys_getpid.c
@@ -1,11 +1,23 @@
+/**************************************************************************
+ * get_pid
+ * 
+ * Output:
+ *		pid_t 	- pid of the current thread's process.
+ *
+ * Description:
+ *		Returns the current process's PID. Cannot fail.
+ *
+ * Last Modified: 5/14/2013 by ginobuzz
+ *
+ **************************************************************************/
 #include <syscall.h>
 
 
 pid_t
 sys_getpid(void) {
-/* Returns the PID of the current process.
-*/ 
+	
 	return curthread->process->pid;
+
 }
 
 
